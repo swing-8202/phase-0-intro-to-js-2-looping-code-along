17d98b9a3d06dfe7f988911a4c72a5e8ab7722c0
Sokung <swing.sophie.8202@gmail.com>
2021-03-29 14:43:12 -0700

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+}
+*/
+/*
+const gifts = ["teddy bear", "drone", "doll"];
+function wrapGifts(gifts) {
+    for (let i = 0; i < gifts.length; i++) {
+        console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    }
+    return gifts;
+}
+wrapGifts(gifts);
+*/
+
+const arrayStringNames = ["Ada", "Brendan", "Ali"];
+const eventName = "birthday";
+const writeCardsArray = [];
+function writeCards(arrayStringNames, eventName, writeCardsArray){
+    for (let i = 0; i < arrayStringNames.length; i++) {
+        console.log(`Thank you, ${arrayStringNames[i]}, for the wonderful ${eventName} gift!`);
+        writeCardsArray +=;
+    }
+    return writeCardsArray;
+}
+writeCards(arrayStringNames, eventName);
